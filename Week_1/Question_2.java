7e2f7b7a13ef426e7bb0298b57bffe921ee04e84
YavuzSelimKiziltas <105879066+YavuzSelimKiziltas@users.noreply.github.com>
2023-03-08 14:25:06 +0300

    Lab #1

---
+++ Week_1/Question_2.java
@@ -0,0 +1,6 @@
+public class Question_2 {
+    
+    public static void main(String[] args){
+        System.out.print(3 + (4 * 2) + 2 * (3 * 3));
+    }
+}
---
+++ Week_1/Question_6.java
@@ -0,0 +1,14 @@
+public class Question_6 {
+
+    public static void main(String[] main) {
+        
+        double km;
+        km = 24 * 1.6;
+
+        double hour;
+        hour = 1 + (40.0 / 60) + (35.0 / (60 * 60));
+
+        System.out.printf("Average speed: %,.14f", km/hour);
+    }
+    
+}
